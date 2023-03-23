9040f53b41aec3f9e4f710a51a0ac7f0c0e1d6f0
Marc Kirchner <mail@marc-kirchner.de>
2023-03-23 11:25:17 +0100

    Add full IPv6 support

--- tcping.c
+++ tcping.c
@@ -37,25 +37,38 @@ void usage();
 int main(int argc, char *argv[])
 {
 
-    int sockfd;
-    int error = 0;
-    int ret;
-    socklen_t errlen;
-    struct timeval timeout;
-    fd_set fdrset, fdwset;
-    int verbosity = 1;
-    int c;
     char *cptr;
+    struct addrinfo hints, *res;
+    int c;
+    int err;
+    socklen_t errlen;
+    int error = 0;
+    long fam;
+    fd_set fdrset, fdwset;
+    int force_ai_family = AF_UNSPEC;
+    char hostname[INET6_ADDRSTRLEN];
+    int ret;
+    char servicename[6];
+    int sockfd;
     long timeout_sec = 0, timeout_usec = 0;
+    struct timeval timeout;
+    int verbosity = 1;
 
     if (argc < 3) {
         usage(argv[0]);
     }
 
-    while ((c = getopt(argc, argv, "qt:u:")) != -1) {
+    while ((c = getopt(argc, argv, "qf:t:u:")) != -1) {
         switch (c) {
         case 'q':
             verbosity = 0;
+            break;
+        case 'f':
+            cptr = NULL;
+            fam = strtol(optarg, &cptr, 10);
+            if (cptr == optarg || (fam != 4 && fam != 6))
+                usage(argv[0]);
+            force_ai_family = fam == 4 ? AF_INET : AF_INET6;
             break;
         case 't':
             cptr = NULL;
@@ -74,36 +87,36 @@ int main(int argc, char *argv[])
             break;
         }
     }
-
-    struct addrinfo hints, *res, *it;
-    int err;
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    
     if (!argv[optind + 1]) {
         usage(argv[0]);
     }
 
-    if ((err = getaddrinfo(argv[optind], argv[optind+1], &hints, &res)) != 0) {
+    /* collect all ipv4 and ipv6 address info */
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = force_ai_family;
+    hints.ai_socktype = SOCK_STREAM;
+    if ((err = getaddrinfo(argv[optind], argv[optind + 1], &hints, &res)) !=
+        0) {
         log(verbosity, stderr, "error: %s\n", gai_strerror(err));
         exit(-1);
     }
+    if ((err = getnameinfo(res->ai_addr, res->ai_addrlen, hostname,
+                           sizeof(hostname), servicename, sizeof(servicename),
+                           NI_NUMERICHOST | NI_NUMERICSERV)) != 0) {
+        log(verbosity, stderr, "error: %s\n", gai_strerror(err));
+        exit(-1);
+    }
+    /* we only look at the first resolution result */
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-    it = res;  /* set iterator to first resolution result */
-
-    /* FIXME: iterate until we find the expected ai_family */
-
-    sockfd = socket(it->ai_family, it->ai_socktype, it->ai_protocol);
-    fprintf(stdout, "ai_fam: %d\n", it->ai_family);
-
+    /* attempt the connection */
     fcntl(sockfd, F_SETFL, O_NONBLOCK);
-    ret = connect(sockfd, it->ai_addr, it->ai_addrlen);
+    ret = connect(sockfd, res->ai_addr, res->ai_addrlen);
     freeaddrinfo(res);
     if (ret) {
         if (errno != EINPROGRESS) {
-            log(verbosity, stderr, "error: %s port %s: %s\n", argv[optind],
-                argv[optind + 1], strerror(errno));
+            log(verbosity, stderr, "error: %s port %s: %s\n", hostname,
+                servicename, strerror(errno));
             return (-1);
         }
 
@@ -119,8 +132,8 @@ int main(int argc, char *argv[])
                                                                : NULL)) == 0) {
             /* timeout */
             close(sockfd);
-            log(verbosity, stdout, "%s port %s user timeout.\n", argv[optind],
-                argv[optind + 1]);
+            log(verbosity, stdout, "%s port %s user timeout.\n", hostname,
+                servicename);
             return (2);
         }
         if (FD_ISSET(sockfd, &fdrset) || FD_ISSET(sockfd, &fdwset)) {
@@ -129,13 +142,13 @@ int main(int argc, char *argv[])
                                   &errlen)) != 0) {
                 /* getsockopt error */
                 log(verbosity, stderr, "error: %s port %s: getsockopt: %s\n",
-                    argv[optind], argv[optind + 1], strerror(errno));
+                    hostname, servicename, strerror(errno));
                 close(sockfd);
                 return (-1);
             }
             if (error != 0) {
-                log(verbosity, stdout, "%s port %s closed.\n", argv[optind],
-                    argv[optind + 1]);
+                log(verbosity, stdout, "%s port %s closed.\n", hostname,
+                    servicename);
                 close(sockfd);
                 return (1);
             }
@@ -146,15 +159,15 @@ int main(int argc, char *argv[])
     }
     /* OK, connection established */
     close(sockfd);
-    log(verbosity, stdout, "%s port %s open.\n", argv[optind],
-        argv[optind + 1]);
+    log(verbosity, stdout, "%s port %s open.\n", hostname, servicename);
     return 0;
 }
 
 void usage(char *prog)
 {
     fprintf(stderr,
-            "error: Usage: %s [-q] [-t timeout_sec] [-u timeout_usec] <host> "
+            "error: Usage: %s [-q] [-f <4|6>] [-t timeout_sec] [-u "
+            "timeout_usec] <host> "
             "<port>\n",
             prog);
     exit(-1);
